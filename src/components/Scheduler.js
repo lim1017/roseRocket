a320b22cacbe747e6e834cfcdcc06448542b698c
tom <lim1017@hotmail.com>
2020-07-23 23:13:47 -0400

    cleanup before push

--- src/components/Scheduler.js
+++ src/components/Scheduler.js
@@ -17,16 +17,15 @@ import {
   TodayButton,
   AppointmentForm,
 } from "@devexpress/dx-react-scheduler-material-ui";
-import DropDown from "./DropDown"
+import DropDown from "./DropDown";
 
-import { RemoveComponent, drivers } from '../helpers/SchedulerHelpers'
-import { BasicLayout } from "./BasicFormLayout"
+import { RemoveComponent, drivers } from "../helpers/SchedulerHelpers";
+import { BasicLayout } from "./BasicFormLayout";
 
-import Moment from 'moment';
-import { extendMoment } from 'moment-range';
+import Moment from "moment";
+import { extendMoment } from "moment-range";
 
 const moment = extendMoment(Moment);
-
 
 // next to work on conflicting times
 //check line 101  compare .startdate/.enddate
@@ -36,14 +35,10 @@ const messages = {
   moreInformationLabel: "",
 };
 
-
-
 const dropDown = (props) => {
   // eslint-disable-next-line react/destructuring-assignment
   return <AppointmentForm.Select {...props} />;
 };
-
-
 
 const SchedulerComponent = () => {
   const [schedulerState, setSchedulerState] = useState({
@@ -51,52 +46,57 @@ const SchedulerComponent = () => {
     currentDate: new Date(),
   });
 
-  const [filteredAppointments, setFilteredAppointments] = useState([])
-  const [activeDriver, setActiveDriver] = useState(drivers[0])
+  const [filteredAppointments, setFilteredAppointments] = useState([]);
+  const [activeDriver, setActiveDriver] = useState(drivers[0]);
 
   const commitChanges = ({ added, changed, deleted }) => {
-    console.log(added)
-    if (added !== undefined){
-      var range = moment.range([added.startDate, added.endDate])
-      console.log(range)
-    } 
+    if (added !== undefined) {
+      var range = moment.range([added.startDate, added.endDate]);
+      console.log(range);
+    }
 
-    if (changed !== undefined){
+    if (changed !== undefined) {
+      var chgAppointmentID = parseInt(Object.keys(changed)[0]);
+      let chgAppointment = filteredAppointments.filter(
+        (appointment) => appointment.id === chgAppointmentID
+      );
 
-      var chgAppointmentID = parseInt(Object.keys(changed)[0])
-      let chgAppointment=filteredAppointments.filter(appointment=>appointment.id === chgAppointmentID)
+      let start = changed[chgAppointmentID].startDate
+        ? changed[chgAppointmentID].startDate
+        : chgAppointment[chgAppointmentID].startDate;
+      let end = changed[chgAppointmentID].endDate
+        ? changed[chgAppointmentID].endDate
+        : chgAppointment[chgAppointmentID].endDate;
 
-      let start = changed[chgAppointmentID].startDate ? changed[chgAppointmentID].startDate : chgAppointment[chgAppointmentID].startDate
-      let end = changed[chgAppointmentID].endDate ? changed[chgAppointmentID].endDate : chgAppointment[chgAppointmentID].endDate
+      var range = moment.range([start, end]);
+    }
 
-      var range = moment.range([start, end])
-    } 
-    
     //check for conflicts
-    const test = filteredAppointments.filter(appointment => appointment.id !== chgAppointmentID )
-    console.log(test)
-    const refilteredAppointments = added ? filteredAppointments : filteredAppointments.filter(appointment => appointment.id !== chgAppointmentID)
+    const refilteredAppointments = added
+      ? filteredAppointments
+      : filteredAppointments.filter(
+          (appointment) => appointment.id !== chgAppointmentID
+        );
 
-    console.log(chgAppointmentID)
-    console.log(refilteredAppointments)
-
-    refilteredAppointments.forEach(appointment =>{
-      console.log(appointment)
-      let range2 = moment.range([appointment.startDate, appointment.endDate])
-      console.log(range2)
-      if(range.overlaps(range2)){
-        alert('conflict!')
+    refilteredAppointments.forEach((appointment) => {
+      console.log(appointment);
+      let range2 = moment.range([appointment.startDate, appointment.endDate]);
+      console.log(range2);
+      if (range.overlaps(range2)) {
+        alert("conflict!");
       }
-    })
+    });
 
-
-    setSchedulerState((state) => { 
-      console.log(state)
+    setSchedulerState((state) => {
+      console.log(state);
       let { data } = state;
       if (added) {
         const startingAddedId =
           data.length > 0 ? data[data.length - 1].id + 1 : 0;
-        data = [...data, { id: startingAddedId, driver: activeDriver, ...added }];
+        data = [
+          ...data,
+          { id: startingAddedId, driver: activeDriver, ...added },
+        ];
       }
       if (changed) {
         data = data.map((appointment) =>
@@ -112,13 +112,13 @@ const SchedulerComponent = () => {
     });
   };
 
-
   useEffect(() => {
-    const filteredAppointments=schedulerState.data.filter(appointment =>appointment.driver == activeDriver)
+    const filteredAppointments = schedulerState.data.filter(
+      (appointment) => appointment.driver == activeDriver
+    );
 
-    setFilteredAppointments(filteredAppointments)
-
-  }, [activeDriver, schedulerState])
+    setFilteredAppointments(filteredAppointments);
+  }, [activeDriver, schedulerState]);
 
   // console.log(schedulerState)
   // console.log(filteredAppointments)
@@ -126,7 +126,12 @@ const SchedulerComponent = () => {
   return (
     <Paper>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <DropDown drivers={drivers} type="Driver" activeDriver={activeDriver} setActiveDriver={setActiveDriver} />
+        <DropDown
+          drivers={drivers}
+          type="Driver"
+          activeDriver={activeDriver}
+          setActiveDriver={setActiveDriver}
+        />
         <button>download driver</button>
       </div>
 
